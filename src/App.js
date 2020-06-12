bfdd5edd226349273bb200ac7f7d280d95838799
Adam Kiss <kiss.adam.a@gmail.com>
2020-06-12 17:08:17 +0300

    Change to async-await

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ const App = () => {
   //This state will be populated with the Axios HTTP response
   const [dogs, setDogs] = useState([]);
 
-  const loadDogs = useCallback(() => {
-    axios.get(`${myDogServerBaseURL}/dogs`).then((response) => {
-      setDogs(response.data);
-    });
+  const loadDogs = useCallback(async () => {
+    const response = await axios.get(`${myDogServerBaseURL}/dogs`);
+    const dogs = response.data;
+    setDogs(dogs);
   }, []);
 
   //The doggies are loaded initially
@@ -21,26 +21,23 @@ const App = () => {
   }, [loadDogs]);
 
   const onCreateNewDog = useCallback(
-    (newDogName) => {
-      axios
-        .post(`${myDogServerBaseURL}/dogs`, {
+    async (newDogName) => {
+      try {
+        await axios.post(`${myDogServerBaseURL}/dogs`, {
           name: newDogName,
-        })
-        .then((result) => {
-          //Reload the doggies to show also the new one
-          loadDogs();
-        })
-        .catch((error) => {
-          /**
-           * If the response status code is 409 - Conflict, then we already have
-           * a doggie with this name.
-           **/
-          if (error.response.status === 409) {
-            alert("Doggie with same name exists!");
-          } else {
-            alert("Unknown error.");
-          }
         });
+        loadDogs();
+      } catch (error) {
+        /**
+         * If the response status code is 409 - Conflict, then we already have
+         * a doggie with this name.
+         **/
+        if (error.response.status === 409) {
+          alert("Doggie with same name exists!");
+        } else {
+          alert("Unknown error.");
+        }
+      }
     },
     [loadDogs]
   );
